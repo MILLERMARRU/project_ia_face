bca68036b66ac34327ff3569f3d610fb888a484c
DoraxDev <samleninvasques@gmail.com>
2025-05-14 16:48:58 -0500

    Creación de la rama Sam_branch. Rama dedicada a modelos experimentales y soporte para OCR

--- db/conexion.py
+++ db/conexion.py
@@ -5,7 +5,7 @@ def obtener_conexion():
     return pymysql.connect(
         host='localhost',         
         user='root',              
-        password='123456',    
+        password='Sam06092003',    
         database='ucss',
         cursorclass=pymysql.cursors.DictCursor
 )
