ac19c4ec1d3d12ffd54fcc64701364ef7c5cbda4
DoraxDev <samleninvasques@gmail.com>
2025-05-19 09:57:56 -0500

    Actualizaciones en rama sam_branch

--- main.py
+++
@@ -1,8 +0,0 @@
-from app.embedings import crear_embedding
-import cv2
-
-# Leer imagen desde archivo o captura
-img = cv2.imread("billie.jpg")
-embedding = crear_embedding(img)
-
-print("Embedding generado:", embedding)
